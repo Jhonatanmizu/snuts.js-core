bed4a158f44e4d6fcb03bae0915c2ac615a68fda
jhonatanmizu <natanjesuss20@gmail.com>
2025-10-16 01:25:44 -0300

    chore: add test runner in lint-staged

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,17 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
 import { configDefaults, defineConfig } from "vitest/config";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "lib"),
+    },
+  },
   test: {
     exclude: [
       ...configDefaults.exclude,
