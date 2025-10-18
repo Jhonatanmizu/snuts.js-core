60dd691a96efd8c9bed3a4dd5ba9cb575cd1d91f
jhonatanmizu <natanjesuss20@gmail.com>
2025-10-18 14:31:39 -0300

    feat: create constants

---
+++ lib/shared/constants.ts
@@ -0,0 +1,17 @@
+const TEST_FILE_PATTERNS = [
+  "**/*.test.js",
+  "**/*.tests.js",
+  "**/*.spec.js",
+  "**/*.specs.js",
+  "**/*test_*.js",
+  "**/*test-*.js",
+  "**/*Spec*.js",
+  "**/*.test.ts",
+  "**/*.tests.ts",
+  "**/*.spec.ts",
+  "**/*.specs.ts",
+  "**/*test_*.ts",
+  "**/*test-*.ts",
+  "**/*Spec*.ts",
+];
+export { TEST_FILE_PATTERNS };
